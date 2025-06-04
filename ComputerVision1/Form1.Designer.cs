7071d5e51e6ac4843704d0f7861a1564cec1528e
Daniele Simoncini <simoncini@codeninja.it>
2025-06-04 13:53:55 +0200

    termine primo esercizio computer vision

--- ComputerVision1/Form1.Designer.cs
+++ ComputerVision1/Form1.Designer.cs
@@ -32,15 +32,17 @@
             PctPreview = new PictureBox();
             DlgApri = new OpenFileDialog();
             TxtRisultato = new TextBox();
+            TrkSoglia = new TrackBar();
             ((System.ComponentModel.ISupportInitialize)PctPreview).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)TrkSoglia).BeginInit();
             SuspendLayout();
             // 
             // BtnCalcola
             // 
             BtnCalcola.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            BtnCalcola.Location = new Point(12, 486);
+            BtnCalcola.Location = new Point(12, 481);
             BtnCalcola.Name = "BtnCalcola";
-            BtnCalcola.Size = new Size(719, 60);
+            BtnCalcola.Size = new Size(955, 60);
             BtnCalcola.TabIndex = 0;
             BtnCalcola.Text = "Calcola";
             BtnCalcola.UseVisualStyleBackColor = true;
@@ -51,7 +53,7 @@
             PctPreview.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             PctPreview.Location = new Point(12, 12);
             PctPreview.Name = "PctPreview";
-            PctPreview.Size = new Size(719, 468);
+            PctPreview.Size = new Size(955, 367);
             PctPreview.SizeMode = PictureBoxSizeMode.StretchImage;
             PctPreview.TabIndex = 1;
             PctPreview.TabStop = false;
@@ -64,25 +66,36 @@
             // TxtRisultato
             // 
             TxtRisultato.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            TxtRisultato.Location = new Point(12, 552);
+            TxtRisultato.Location = new Point(12, 547);
             TxtRisultato.Multiline = true;
             TxtRisultato.Name = "TxtRisultato";
             TxtRisultato.ScrollBars = ScrollBars.Vertical;
-            TxtRisultato.Size = new Size(719, 322);
+            TxtRisultato.Size = new Size(955, 322);
             TxtRisultato.TabIndex = 2;
             TxtRisultato.WordWrap = false;
+            // 
+            // TrkSoglia
+            // 
+            TrkSoglia.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            TrkSoglia.Location = new Point(12, 385);
+            TrkSoglia.Maximum = 255;
+            TrkSoglia.Name = "TrkSoglia";
+            TrkSoglia.Size = new Size(955, 90);
+            TrkSoglia.TabIndex = 3;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(743, 886);
+            ClientSize = new Size(979, 881);
+            Controls.Add(TrkSoglia);
             Controls.Add(TxtRisultato);
             Controls.Add(PctPreview);
             Controls.Add(BtnCalcola);
             Name = "Form1";
             Text = "Computer Vision 1";
             ((System.ComponentModel.ISupportInitialize)PctPreview).EndInit();
+            ((System.ComponentModel.ISupportInitialize)TrkSoglia).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -93,5 +106,6 @@
         private PictureBox PctPreview;
         private OpenFileDialog DlgApri;
         private TextBox TxtRisultato;
+        private TrackBar TrkSoglia;
     }
 }
--- ComputerVision1/Form1.cs
+++ ComputerVision1/Form1.cs
@@ -1,3 +1,6 @@
+using ComputerVision1.Strutture;
+using System.Drawing.Imaging;
+
 namespace ComputerVision1
 {
     public partial class Form1 : Form
@@ -17,8 +20,41 @@ namespace ComputerVision1
             {
                 PctPreview.Image =
                     Image.FromFile(DlgApri.FileName);
+                Bitmap immagine = (Bitmap)PctPreview.Image;
+                PercentualeColori colori = Scomponi(immagine);
                 TxtRisultato.Text += $"Immagine caricata nelle dimensioni {PctPreview.Size}!";
+                TxtRisultato.Text += $"Rosso:\t{colori.PercR}";
+                TxtRisultato.Text += $"Verde:\t{colori.PercG}";
+                TxtRisultato.Text += $"Blu:\t{colori.PercB}";
             }
+        }
+
+        private PercentualeColori Scomponi(Bitmap target)
+        {
+            Bitmap canaleR = new Bitmap(target.Width, target.Height);
+            Bitmap canaleG = new Bitmap(target.Width, target.Height);
+            Bitmap canaleB = new Bitmap(target.Width, target.Height);
+            PercentualeColori risultato = new PercentualeColori(target.Size);
+            for(int x=0; x < target.Width; x++)
+            {
+                for(int y=0; y < target.Height; y++)
+                {
+                    Color casella = target.GetPixel(x, y);
+                    risultato.R += casella.R;
+                    risultato.G += casella.G;
+                    risultato.B += casella.B;
+                    if(casella.R > TrkSoglia.Value)
+                        canaleR.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                    if(casella.G > TrkSoglia.Value)
+                        canaleG.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                    if(casella.B > TrkSoglia.Value)
+                        canaleB.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                }
+            }
+            canaleR.Save("canaleR.jpg", ImageFormat.Jpeg);
+            canaleG.Save("canaleG.jpg", ImageFormat.Jpeg);
+            canaleB.Save("canaleB.jpg", ImageFormat.Jpeg);
+            return risultato;
         }
     }
 }
