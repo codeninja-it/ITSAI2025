f8e75c54c1a13780fb9724a545aa2d768cc67106
Daniele Simoncini <simoncini@codeninja.it>
2025-06-09 13:04:53 +0200

    inizio esercizio analisi del traffico

---
+++ Traffico/Program.cs
@@ -0,0 +1,12 @@
+﻿using Accord.Video;
+
+MJPEGStream video = new MJPEGStream("https://video.autostrade.it/video-mp4_hq/dt4/53c1cb2d-9a8d-4882-b3e5-dab0528ebe5d-29.mp4");
+
+video.NewFrame += Video_NewFrame;
+
+video.Start();
+
+void Video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+{
+    Console.WriteLine(video.FramesReceived);
+}
