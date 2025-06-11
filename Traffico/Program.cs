86492704a998f36be2461e692fd245549689dd73
Daniele Simoncini <simoncini@codeninja.it>
2025-06-11 15:12:06 +0200

    crezione sintentiche con thresholding su Otsu e cancellazione del background

--- Traffico/Program.cs
+++ Traffico/Program.cs
@@ -6,19 +6,26 @@ if (!Directory.Exists("sintetiche"))
     Directory.CreateDirectory("sintetiche");
 
 VideoCapture video = new VideoCapture("https://video.autostrade.it/video-mp4_hq/dt4/53c1cb2d-9a8d-4882-b3e5-dab0528ebe5d-29.mp4");
-Mat frame = new Mat();
+Mat sfondo = new Mat();
 int i = 0;
 while (video.Grab())
 {
+    Mat frame = new Mat();
     video.Retrieve(frame);
-    CvInvoke.CvtColor(frame, frame, Emgu.CV.CvEnum.ColorConversion.Rgb2Hls);
-    //CvInvoke.CvtColor(frame, frame, Emgu.CV.CvEnum.ColorConversion.Rgb2Gray);
+    CvInvoke.CvtColor(frame, frame, Emgu.CV.CvEnum.ColorConversion.Rgb2Gray);
 
-    frame
-        .ToBitmap()
-        .Save(
-            Path.Combine("sintetiche", $"f_{i}.jpg"),
-            ImageFormat.Jpeg
-        );
+    if (!sfondo.IsEmpty)
+    {
+        Mat differenza = new Mat();
+        CvInvoke.AbsDiff(frame, sfondo, differenza);
+        CvInvoke.Threshold(differenza, differenza, 50, 255, Emgu.CV.CvEnum.ThresholdType.Otsu);
+        differenza
+            .ToBitmap()
+            .Save(
+                Path.Combine("sintetiche", $"f_{i}.jpg"),
+                ImageFormat.Jpeg
+            );
+    }
     i++;
+    sfondo = frame;
 }
